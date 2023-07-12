5d6ab7a96f9c120996c948546459e9b5e3f4bd2f
Filipe André <flipeandre12@gmail.com>
2023-07-12 17:35:16 -0300

    feat: operadores booleanos

--- operadores_booleanos.js
+++ operadores_booleanos.js
@@ -9,11 +9,35 @@ Operadores booleanos
 */
 
 const numero = 20;
+
 console.log(numero == 20); // true
+
 // == (comparar apenas o conteúdo) ou === (também compara o tipo)
-// Sempre utilizar ===, para evitar bugs
+// Sempre utilizar ===, para evitar bugs.
 
 console.log(numero == '20'); // true
 console.log(numero === '20'); // false
 
-// Sempre usar o !== para verificar o tipo novamente
+// limpar terminal
+console.clear()
+
+// Sempre usar o !== para verificar pois também comparar por tipo.
+console.log(numero != '20');
+console.log(numero !== '20');
+
+console.clear()
+
+// Conjunções lógicas
+/* 
+    AND (&&)
+    OR (||)
+    NOT (!)
+*/
+
+let idade = 80;
+let posuiCnh = true;
+
+// se a idade for maior ou igual a 18 e menor que 80 e possuir Cnh
+let podeDirigir = idade >= 18 && idade < 80 && posuiCnh != false ? "Pode dirigir" :"Não pode dirigir";
+
+console.log(podeDirigir);
