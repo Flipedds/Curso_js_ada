6e4b5299c67f66d6c4325f738b43404a3a5f7a5f
Filipe André <flipeandre12@gmail.com>
2023-07-12 12:57:34 -0300

    feat: biblioteca Math

--- operadores_aritmeticos.js
+++ operadores_aritmeticos.js
@@ -23,3 +23,17 @@ console.log("Soma: ", soma);
 console.log("Subtração: ", subtra);
 console.log("Divisão: ", div);
 console.log("Resto da divisão: ", resto);
+
+// Precedencia de operadores
+console.log(3+3*7); // multiplicação posteriormente a soma;
+
+// Biblioteca Math
+// com funções para cálculos matemáticos avançados
+
+// Pi
+const pi = Math.PI;
+console.log(pi);
+
+// Raiz quadrada
+const raiz = Math.sqrt(16)
+console.log(raiz);
